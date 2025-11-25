625cc8518c36c10ed91e81dc6d03cea14f192d81
Iya Rahmani <eiliarhmani1177@gmail.com>
2025-11-25 13:11:24 -0800

    Clean everything ready to start progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
 import Image from "next/image";
-import { Button } from "@mui/material";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-     <Button className="bg-amber-600">Text</Button>
-     <Button variant="outlined">Outlined</Button>
+    <div>
+      <main>
       </main>
     </div>
   );
